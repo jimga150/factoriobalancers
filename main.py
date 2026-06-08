a2162af5be93bb0afe58a44b371646ea93d7afc6
jimga150 <jimga150@users.noreply.github.com>
2026-06-08 16:23:44 -0400

    Make output balance test

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ from Balancer import Balancer
 # balancer = Balancer_Book.make_2x1_pri_in()
 # balancer = Balancer_Book.make_3x1()
 # balancer = Balancer_Book.make_4x4_universal()
-balancer = Balancer_Book.make_real_3x1_reduced()
+# balancer = Balancer_Book.make_real_3x1_reduced()
+balancer = Balancer_Book.make_4x3()
 
 num_outputs = balancer.get_num_outputs()
 num_inputs = balancer.get_num_inputs()
@@ -23,6 +24,7 @@ exp_balance_coeff = num_outputs/num_inputs if num_outputs < num_inputs else num_
 
 num_inputs_1dry = num_inputs-1
 exp_balance_coeff_1dry = 1/num_outputs
+exp_balance_coeff_1blocked = 1/num_inputs
 
 # test drying each input individually
 for in_belt in balancer.get_inputs():
@@ -47,6 +49,31 @@ for in_belt in balancer.get_inputs():
         print(f"Balancer unbalanced when input {in_belt.source} is dry.")
 
     in_belt.enabled = True
+
+# test blocking each output
+for out_belt in balancer.get_outputs():
+    out_belt.enabled = False
+
+    print(f"Calculating balance with {out_belt.dest} blocked")
+
+    balancer.calc_balance()
+    balancer.render(f"Block_{out_belt.dest}")
+
+    is_balanced = True
+    for enabled_out_belt in balancer.get_outputs():
+        if not enabled_out_belt.enabled:
+            continue
+        for name, frac in enabled_out_belt.supply_balance.items():
+            if abs(frac - exp_balance_coeff_1blocked) > common.diff_threshold_verif:
+                print(f"Error on {enabled_out_belt.dest}: expected balance of input {name} to be "
+                      f"{exp_balance_coeff_1blocked:.{common.decimals_iter}f}, got {frac:.{common.decimals_iter}f}"
+                      f" (diff > {common.diff_threshold_verif})")
+                is_balanced = False
+
+    if not is_balanced:
+        print(f"Balancer unbalanced when output {out_belt.dest} is blocked.")
+
+    out_belt.enabled = True
 
 balancer.calc_balance()
 balancer.render()
