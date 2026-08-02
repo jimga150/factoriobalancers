5f70e6d31b94ae3b9dc5dae72f35675d9ec247af
jimga150 <jimga150@users.noreply.github.com>
2026-08-02 19:16:47 -0400

    reuse balancer objects

--- z3BalancerTests.py
+++ z3BalancerTests.py
@@ -5,40 +5,37 @@ import Balancer_Book
 
 
 class MyTestCase(unittest.TestCase):
+
+    balancer22 = Balancer_Book.make_2x2()
+    balancer44 = Balancer_Book.make4x4()
+    balancer44TU = Balancer.combine_endtoend(balancer44)
+
     def test_TU(self):
-        balancer44 = Balancer_Book.make4x4()
-        balancer44TU = Balancer.combine_endtoend(balancer44)
-        _, _, is_tu = Balancer_Book.test_balance_z3(balancer44TU)
+        _, _, is_tu = Balancer_Book.test_balance_z3(self.balancer44TU)
         self.assertEqual(is_tu, True)
 
     def test_not_TU(self):
-        balancer44 = Balancer_Book.make4x4()
-        _, _, is_tu = Balancer_Book.test_balance_z3(balancer44)
+        _, _, is_tu = Balancer_Book.test_balance_z3(self.balancer44)
         self.assertEqual(is_tu, False)
 
     def test_not_input_balanced(self):
-        balancer44 = Balancer_Book.make4x4()
-        inb, _, _ = Balancer_Book.test_balance_z3(balancer44)
+        inb, _, _ = Balancer_Book.test_balance_z3(self.balancer44)
         self.assertEqual(inb, False)
 
     def test_not_output_balanced(self):
-        balancer44 = Balancer_Book.make4x4()
-        _, outb, _ = Balancer_Book.test_balance_z3(balancer44)
+        _, outb, _ = Balancer_Book.test_balance_z3(self.balancer44)
         self.assertEqual(outb, False)
 
     def test_2x2_TU(self):
-        balancer = Balancer_Book.make_2x2()
-        _, _, is_tu = Balancer_Book.test_balance_z3(balancer)
+        _, _, is_tu = Balancer_Book.test_balance_z3(self.balancer22)
         self.assertEqual(is_tu, True)
 
     def test_2x2_input_balanced(self):
-        balancer = Balancer_Book.make_2x2()
-        inb, _, _ = Balancer_Book.test_balance_z3(balancer)
+        inb, _, _ = Balancer_Book.test_balance_z3(self.balancer22)
         self.assertEqual(inb, True)
 
     def test_2x2_output_balanced(self):
-        balancer = Balancer_Book.make_2x2()
-        _, outb, _ = Balancer_Book.test_balance_z3(balancer)
+        _, outb, _ = Balancer_Book.test_balance_z3(self.balancer22)
         self.assertEqual(outb, True)
 
 
