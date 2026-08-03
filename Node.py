f8a2345c5294ee78f2eab8a7da572bfc997ad163
jimga150 <jimga150@users.noreply.github.com>
2026-08-03 18:04:37 -0400

    use ID assignment for nodes and use threadLock

--- Node.py
+++ Node.py
@@ -1,12 +1,37 @@
+import threading
+import time
+
+threadLock = threading.Lock()
+node_idx = 0
+copy_delay = 0
 
 class Node:
     def __init__(self):
         self.name = ""
+        self.id = None
+        self.get_new_id()
+
+    def get_new_id(self):
+        with threadLock:
+            global node_idx
+            self.id = node_idx
+            if copy_delay > 0:
+                time.sleep(copy_delay)
+            node_idx += 1
 
     def __str__(self):
         if self.name != "":
             return self.name
-        return str(hash(self))[-4:]
+        # return hex(hash(self))[-4:]
+        return str(self.id)
+
+    def __copy__(self):
+        ans = Node()
+        ans.name = self.name
+        return ans
+
+    def __deepcopy__(self, memo):
+        return self.__copy__()
 
     def __hash__(self):
         return hash(str(id(self)))
