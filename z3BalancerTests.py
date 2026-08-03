5ab6fcd55245eb69ac64fe5ae1dce8dcdfa9c5b5
jimga150 <jimga150@users.noreply.github.com>
2026-08-02 23:49:43 -0400

    generalize balancer tests

--- z3BalancerTests.py
+++ z3BalancerTests.py
@@ -10,32 +10,25 @@ class MyTestCase(unittest.TestCase):
     balancer44 = Balancer.combine_sidebyside(balancer22)
     balancer44TU = Balancer.combine_endtoend(balancer44)
 
+    # def test_play(self):
+    #     self.assertTrue(True)
+    #     self.assertEqual(Balancer_Book.test_input_balanced_z3(self.balancer44), False)
+
+    def test_2x2(self):
+        self.runtest_balancer(self.balancer22, True, True, True, True, True)
+
+    def test_4x4(self):
+        self.runtest_balancer(self.balancer44, False, True, False, True, False)
+
     def test_4x4TU(self):
-        self.assertEqual(Balancer_Book.test_tu_z3(self.balancer44TU), True)
+        self.runtest_balancer(self.balancer44TU, True, True, False, True, False)
 
-    def test_4x4_not_TU(self):
-        self.assertEqual(Balancer_Book.test_tu_z3(self.balancer44), False)
-
-    def test_4x4_not_input_balanced(self):
-        self.assertEqual(Balancer_Book.test_input_balanced_z3(self.balancer44), False)
-
-    def test_4x4_not_output_balanced(self):
-        self.assertEqual(Balancer_Book.test_output_balanced_z3(self.balancer44), False)
-
-    def test_2x2_TU(self):
-        self.assertEqual(Balancer_Book.test_tu_z3(self.balancer22), True)
-
-    def test_2x2_pi_balanced(self):
-        self.assertEqual(Balancer_Book.test_partial_input_balanced_z3(self.balancer22), True)
-
-    def test_2x2_input_balanced(self):
-        self.assertEqual(Balancer_Book.test_input_balanced_z3(self.balancer22), True)
-
-    def test_2x2_po_balanced(self):
-        self.assertEqual(Balancer_Book.test_partial_output_balanced_z3(self.balancer22), True)
-
-    def test_2x2_output_balanced(self):
-        self.assertEqual(Balancer_Book.test_output_balanced_z3(self.balancer22), True)
+    def runtest_balancer(self, balancer: Balancer, tu: bool, pi: bool, fi: bool, po: bool, fo: bool):
+        self.assertEqual(Balancer_Book.test_tu_z3(balancer), tu)
+        self.assertEqual(Balancer_Book.test_partial_input_balanced_z3(balancer), pi)
+        self.assertEqual(Balancer_Book.test_input_balanced_z3(balancer), fi)
+        self.assertEqual(Balancer_Book.test_partial_output_balanced_z3(balancer), po)
+        self.assertEqual(Balancer_Book.test_output_balanced_z3(balancer), fo)
 
 
 if __name__ == '__main__':
