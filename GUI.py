d12eab3a3a39e42ba9b055cbd41d95688474d883
jimga150 <jimbosned@gmail.com>
2026-09-11 23:07:25 -0400

    add scale to help me see the whole thing for now

--- GUI.py
+++ GUI.py
@@ -349,6 +349,7 @@ class GUI(QtWidgets.QMainWindow):
         self.setPalette(p)
 
         with QPainter(self) as p:
+            p.scale(0.8, 0.8)
             p.translate(0, 16)
             for y in range(0, self.bp.height):
                 for x in range(0, self.bp.width):
